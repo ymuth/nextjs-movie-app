6bd23abf4ba8d233eeefe930b95f7d73160517eb
ymuth <135130958+ymuth@users.noreply.github.com>
2026-06-22 23:39:22 +0100

    added font, created intial homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-
+import {bebasNeue} from "@/lib/fonts"
 
 export default function Home() {
   return (
-    <div >
-      
+    <div className="flex flex-col text-center justify-center m-5 p-5" >
+      <h1 className={`text-9xl font-semibold border-b-1 ${bebasNeue.className}`}>MUGZ Movies</h1>
+      <p className="mt-5 text-xl">Welcome to <strong>MUGZ Movies</strong>.</p>
+      <p className="">Where we bring the best movie recommendations to <strong className="underline">you!</strong></p>
     </div>
   );
 }
