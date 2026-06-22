62739fb39f8d31266bde4ca9e4bd679077778148
ymuth <135130958+ymuth@users.noreply.github.com>
2026-06-22 22:54:55 +0100

    added first pages, implemented navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return(
+        <div>
+            <h1 className="text-3xl p-2 m-10"> About us. </h1>
+        </div>
+    )
+}
---
+++ components/ui/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <nav className="flex justify-between align-center py-2 px-4 border-b-2">
+
+            <div>
+                <Link href={"/"}>Movie App</Link>
+            </div>
+
+            <div className="flex gap-4">
+                <Link href={"/movies"}>Movies</Link>
+                <Link href={"/favourites"}>Favourites</Link>
+                <Link href={"/about"}>About</Link>
+            </div>
+
+
+        </nav>
+    )
+}
