ece0fe7e16c782d3da41ad9b62cf02c2c81219e8
ymuth <135130958+ymuth@users.noreply.github.com>
2026-06-29 15:48:46 +0100

    movie pages update (gird image desc)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [new URL('https://image.tmdb.org/t/p/w500/**'), new URL ('https://image.tmdb.org/t/p/w1280/**')],
+  },
+
 };
 
 export default nextConfig;
