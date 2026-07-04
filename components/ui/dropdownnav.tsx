29377f2382d0d4cef17e742ca4b2eb928ae13741
ymuth <135130958+ymuth@users.noreply.github.com>
2026-07-04 19:45:16 +0100

    fixed nav bar mobile navigation

--- components/ui/dropdownnav.tsx
+++ components/ui/dropdownnav.tsx
@@ -2,25 +2,37 @@
 import Link from "next/link";
 import { useState } from "react";
 import SearchBar from "./searchbar";
+import { bebasNeue } from "@/lib/fonts";
 
 
 export default function DropdownNav() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className="relative">
+        <div className="bg-[#161616] w-full border-b-2">
 
-            <button onClick={() => setOpen(!open)}>
-                ☰
-            </button>
+            {/* Top Bar */}
+            <div className="flex w-full justify-between items-center py-2 px-4">
 
+                <div className={`${bebasNeue.className} text-xl`}>
+                    <Link href={"/"} onClick={() => setOpen(false)}>Mugz Movies</Link>
+                </div>
+
+                {/* Toggle */}
+                <button className="text-xl p-2" onClick={() => setOpen(!open)}>
+                    {open ? "✕" : "☰"}
+                </button>
+
+            </div>
+
+            {/* Expandable Menu */}
             {open && (
-                <div className="absolute right-0   bg-[#161616] border rounded-lg shadow-lg">
-                    <div className="flex flex-col gap-3 p-4">
+                <div className="w-full bg-[#121212] border-t transition-all">
+                    <div className="flex flex-col p-4 gap-4">
                         <SearchBar />
-                        <Link href={"/movies"}>Movies</Link>
-                        <Link href={"/favourites"}>Favourites</Link>
-                        <Link href={"/about"}>About</Link>
+                        <Link className="hover:underline" href="/movies" onClick={() => setOpen(false)}>Movies</Link>
+                        <Link className="hover:underline" href="/favourites" onClick={() => setOpen(false)}>Favourites</Link>
+                        <Link className="hover:underline" href="/about" onClick={() => setOpen(false)}>About</Link>
                     </div>
                 </div>
             )}
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,22 +5,27 @@ import DropdownNav from "./dropdownnav";
 
 export default function NavBar() {
     return (
-        <nav className="bg-[#161616] relative z-20 flex justify-between align-middle py-2 px-4 border-b-2">
-
-            <div className={`${bebasNeue.className} text-xl`}>
-                <Link href={"/"}>Mugz Movies</Link>
-            </div>
+        <nav className="relative z-20">
 
             {/* Desktop */}
-            <div className="hidden md:flex items-center gap-4">
-                <SearchBar/>
-                <Link href={"/movies"}>Movies</Link>
-                <Link href={"/favourites"}>Favourites</Link>
-                <Link href={"/about"}>About</Link>
+            <div className="hidden md:flex w-full bg-[#161616] relative z-20  align-middle py-2 px-4 border-b-2">
+
+                <div className={`${bebasNeue.className} text-xl`}>
+                    <Link href={"/"}>Mugz Movies</Link>
+                </div>
+
+                <div className=" flex items-center gap-4 ml-auto">
+                    <div className=""><SearchBar /></div>
+                    <Link className="hover:underline" href={"/movies"}>Movies</Link>
+                    <Link className="hover:underline" href={"/favourites"}>Favourites</Link>
+                    <Link className="hover:underline" href={"/about"}>About</Link>
+                </div>
+
             </div>
 
+            {/* Mobile */}
             <div className="md:hidden">
-                <DropdownNav/>
+                <DropdownNav />
             </div>
 
 
